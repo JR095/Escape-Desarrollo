a4fe1485fd881775e80f5688f6843e51fb120162
JR095 <jurgenbs01@gmail.com>
2024-08-24 15:20:29 -0600

    Configuration files

---
+++ escape-desarrollo-frontend/src/main.jsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <h1 className="text-3xl font-bold underline">Escape Team</h1>
+)
