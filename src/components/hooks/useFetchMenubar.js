d52a79e1164e288679a61a485faa83069d50b98f
AsuncionPD <asuncion.pena@ucr.ac.cr>
2024-09-04 01:43:22 -0600

    Modified incorrect properties names
    Se modificarón los nombres de las propiedades para que se adapten al componente React

--- src/components/hooks/useFetchMenubar.js
+++ src/components/hooks/useFetchMenubar.js
@@ -1,1 +1,27 @@
-{isMobile ? <Menubar /> : <Sidebar />}
+import { useEffect, useState } from "react";
+
+export const useFetchMenubar = () => {
+
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            
+            if (window.innerWidth <= 768) {
+                setIsMobile(true);
+            } else {
+                setIsMobile(false);
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+        handleResize(); 
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+
+    },);
+
+    return { isMobile };
+};
