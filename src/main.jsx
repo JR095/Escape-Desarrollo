f61674f4d24b07d120d4e0888e8ce1405664d8c6
Nahomi27 <lawrencenahomi@gmail.com>
2024-09-06 02:31:01 -0600

    ...

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { SearchDropdown } from './components/search/SearchDropdown.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
       <BrowserRouter>
-        <SearchDropdown/>
+        <App/>
       </BrowserRouter>
   </>
 )
