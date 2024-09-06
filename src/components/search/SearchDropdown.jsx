7960b00c9ba3526bf84f9b912d0bc1ef65837f90
Nahomi27 <lawrencenahomi@gmail.com>
2024-09-06 01:42:17 -0600

    SearchDropdown in mobile

--- src/components/search/SearchDropdown.jsx
+++ src/components/search/SearchDropdown.jsx
@@ -1,134 +1,112 @@
+import React, { useState } from 'react';
 import "../../index.css";
-import React, { useState, useEffect, useRef } from 'react';
+import { useSearchDropdown } from '../hooks/useSearchDropdown';
 
-const SearchDropdown = () => {
-  // Estados para manejar el texto de búsqueda, sugerencias y visibilidad de las búsquedas recientes
-  const [query, setQuery] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
-  const [recentSearches, setRecentSearches] = useState([]);
-  const [showRecentSearches, setShowRecentSearches] = useState(false);
 
-  // Referencia para el contenedor principal
-  const searchRef = useRef(null);
+export function SearchDropdown() {
+  const [isMobileSearchVisible, setIsMobileSearchVisible] = useState(false);
+  const items = ['Restaurantes', 'Bares', 'Cafés', 'Hoteles', 'Cabañas', 'Parques'];
 
-  // Lista de elementos para sugerencias (simulando elementos comunes)
-  const items = ['Profile', 'Settings', 'Dashboard', 'Messages', 'Notifications', 'Logout'];
+  const {
+    query, suggestions, recentSearches, showRecentSearches, isInputFocused, searchRef, 
+    handleInputChange, handleSuggestionClick, handleFocus, handleBlur, handleBackButtonClick,
+  } = useSearchDropdown(items);
 
-  // Manejar cambios en el campo de entrada
-  const handleInputChange = (event) => {
-    const value = event.target.value;
-    setQuery(value);
-
-    // Filtrar sugerencias basadas en el texto ingresado
-    if (value === '') {
-      setSuggestions([]); // Si no hay texto, no mostrar sugerencias
-      setShowRecentSearches(true); // Mostrar búsquedas recientes si no hay texto
-    } else {
-      const filteredSuggestions = items.filter((item) =>
-        item.toLowerCase().includes(value.toLowerCase())
-      );
-      setSuggestions(filteredSuggestions); // Mostrar sugerencias filtradas
-      setShowRecentSearches(false); // Ocultar búsquedas recientes cuando se filtran sugerencias
-    }
-  };
-
-  // Manejar la selección de una sugerencia
-  const handleSuggestionClick = (suggestion) => {
-    setQuery(suggestion);
-    setSuggestions([]);
-    updateRecentSearches(suggestion);
-  };
-
-  // Actualizar búsquedas recientes
-  const updateRecentSearches = (searchTerm) => {
-    const updatedRecentSearches = [searchTerm, ...recentSearches.filter((item) => item !== searchTerm)];
-    
-    // Mantener solo las últimas 5 búsquedas
-    if (updatedRecentSearches.length > 5) {
-      updatedRecentSearches.pop();
-    }
-    
-    setRecentSearches(updatedRecentSearches);
-    setShowRecentSearches(false);
-  };
-
-  // Mostrar búsquedas recientes al enfocar el campo de entrada
-  const handleFocus = () => {
-    setShowRecentSearches(true);
-  };
-
-  // Manejar clics fuera del componente para ocultar las búsquedas recientes
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (searchRef.current && !searchRef.current.contains(event.target)) {
-        setShowRecentSearches(false);
-        setSuggestions([]); // Asegurarse de que se ocultan las sugerencias también
-      }
-    };
-
-    document.addEventListener('click', handleClickOutside, true);
-    return () => {
-      document.removeEventListener('click', handleClickOutside, true);
-    };
-  }, []);
-
-  // Función para manejar el clic en el botón de devolverse
-  const handleBackButtonClick = () => {
-    setShowRecentSearches(false);
+  const toggleMobileSearch = () => {
+    setIsMobileSearchVisible(!isMobileSearchVisible);
   };
 
   return (
-    <div ref={searchRef} className="relative w-full max-w-xs mx-auto mt-10">
-      <div className="relative">
-        {/* Campo de entrada para búsqueda */}
-        <input
-          type="text"
-          value={query}
-          onChange={handleInputChange}
-          onFocus={handleFocus}
-          className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-          placeholder="Search..."
-        />
+    <div ref={searchRef} className={`relative w-full max-w-xs mx-auto mt-10 p-4 rounded-md ${isInputFocused ? 'lg:bg-white border-none lg:shadow-md' : ''}`}>
+
+      {!isMobileSearchVisible && (
+        <button
+          type="button"
+          className="lg:hidden block fixed top-4 right-4 z-20" 
+          onClick={toggleMobileSearch}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+          </svg>
+        </button>
+      )}
+
+      <div
+        className={`fixed top-0 right-0 z-10 h-full w-full bg-white lg:static lg:translate-x-0 transition-transform duration-300 ${isMobileSearchVisible ? 'translate-x-0' : 'translate-x-full'
+          }`}
+      >
+        <form className="flex items-center px-4 py-2 rounded-md shadow-md">
+    
+          {(isMobileSearchVisible || isInputFocused) && (
+            <button
+              type="button"
+              onClick={isMobileSearchVisible ? toggleMobileSearch : handleBackButtonClick}
+              className="mr-3 px-2 py-1 bg-gray-100 rounded-lg hover:bg-gray-200"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-[1.20rem]">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+              </svg>
+            </button>
+          )}
+
+          <input
+            type="text"
+            value={query}
+            onChange={handleInputChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            className="w-full border-none outline-none"
+            placeholder="Buscar"
+          />
+
+          <button type="submit" className="block">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </button>
+        </form>
+
+        {showRecentSearches && (
+          <ul className="left-0 right-0 w-full mt-2">
+            {recentSearches.length > 0 ? (
+              <>
+                <h2 className="px-4 py-2 font-bold text-lg">Recientes</h2>
+                {recentSearches.map((search, index) => (
+                  <li
+                    key={index}
+                    className="px-4 py-2 cursor-pointer hover:bg-gray-200 rounded-md flex justify-between"
+                    onClick={() => handleSuggestionClick(search)}
+                  >
+                    {search}
+                    <button>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                      </svg>
+                    </button>
+                  </li>
+                ))}
+              </>
+            ) : (
+              <li className="px-4 py-2 text-gray-500 text-center">No hay búsquedas recientes</li>
+            )}
+          </ul>
+        )}
+
+        {suggestions.length > 0 && (
+          <ul className="left-0 right-0 w-full mt-2">
+            {suggestions.map((suggestion, index) => (
+              <li
+                key={index}
+                className="px-4 py-2 cursor-pointer hover:bg-gray-100 rounded-md"
+                onClick={() => handleSuggestionClick(suggestion)}
+              >
+                {suggestion}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-
-      {/* Botón de devolverse */}
-      <button onClick={handleBackButtonClick}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-        </svg>
-      </button>
-
-      {/* Lista desplegable de búsquedas recientes o sugerencias */}
-      {showRecentSearches && recentSearches.length > 0 && (
-        <ul className="absolute left-0 right-0 w-full mt-2 bg-white border border-gray-300 rounded-lg shadow-lg">
-          {recentSearches.map((search, index) => (
-            <li
-              key={index}
-              className="px-4 py-2 cursor-pointer hover:bg-gray-200"
-              onClick={() => handleSuggestionClick(search)}
-            >
-              {search}
-            </li>
-          ))}
-        </ul>
-      )}
-
-      {/* Mostrar sugerencias filtradas */}
-      {suggestions.length > 0 && (
-        <ul className="absolute left-0 right-0 w-full mt-2 bg-white border border-gray-300 rounded-lg shadow-lg">
-          {suggestions.map((suggestion, index) => (
-            <li
-              key={index}
-              className="px-4 py-2 cursor-pointer hover:bg-gray-200"
-              onClick={() => handleSuggestionClick(suggestion)}
-            >
-              {suggestion}
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   );
-};
+}
 
-export default SearchDropdown;
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import SearchDropdown from './components/search/SearchDropdown.jsx'
+import { SearchDropdown } from './components/search/SearchDropdown.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
