a288392583c4af3467cc16757d745606c6388df4
TaylorYT29 <taylorduransibaja@gmail.com>
2024-09-22 12:27:40 -0700

    Dark-mode profile 25%

--- src/components/profile/PersonalInformation.jsx
+++ src/components/profile/PersonalInformation.jsx
@@ -46,33 +46,33 @@ export function PersonalInformation() {
                     <SearchDropdown />
                 </div>
 
-                <div className="grid grid-cols-1 items-center lg:grid-cols-2">
+                <div className="grid grid-cols-1 items-center lg:grid-cols-2 dark:bg-[#2a2a2a]">
                     <div>
                         <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="Profile_Img" className="rounded-full h-[7rem] w-[7rem] mt-[2rem]" />
-                        <h3 className="font-bold lg:text-2xl text-xl mt-[2rem]">Soda Maria</h3>
+                        <h3 className="font-bold lg:text-2xl text-xl mt-[2rem] dark:text-white">Soda Maria</h3>
                         <h4 className=" text-[#606060] font-semibold  pt-[1rem] lg:hidden">Soda</h4>
-                        <div className="col-span-3 text-left lg:pt-[2rem] pt-[1rem]">
+                        <div className="col-span-3 text-left lg:pt-[2rem] pt-[1rem] dark:text-white">
                             <p>Abrimos a las 6:00 p.m y cerramos a las 2:30a.m <br />Para pedidos : 83287126 - 62865808</p>
                         </div>
                     </div>
 
                     <div className="grid lg:grid-cols-3 text-center lg:align-center">
                         <div className="hidden lg:block">
-                            <h4 className=" text-[#606060] font-semibold lg:py-[0.5rem] pt-[2rem]">Soda</h4>
-                            <p className="pt-[2rem]">1</p>
-                            <h4>Publicaciones</h4>
+                            <h4 className=" text-[#606060] font-semibold lg:py-[0.5rem] pt-[2rem] dark:text-white">Soda</h4>
+                            <p className="pt-[2rem] dark:text-white">1</p>
+                            <h4 className="dark:text-white">Publicaciones</h4>
                         </div>
 
                         <div className="hidden lg:block">
-                            <button className="bg-[#E0E1E3] font-semibold rounded-xl px-[4rem] py-[0.5rem] mt-[1rem] lg:mt-[0rem]">Editar</button>
-                            <p className="pt-[2rem]">1</p>
-                            <h4>Seguidores</h4>
+                            <button className="bg-[#E0E1E3] font-semibold rounded-xl px-[4rem] py-[0.5rem] mt-[1rem] lg:mt-[0rem] dark:text-white">Editar</button>
+                            <p className="pt-[2rem] dark:text-white">1</p>
+                            <h4 className="dark:text-white">Seguidores</h4>
                         </div>
 
                         <div className="hidden lg:block">
-                            <button className="bg-[#E0E1E3] font-semibold rounded-xl px-[4rem] py-[0.5rem] mt-[1rem] lg:mt-[0rem]">Compartir</button>
-                            <p className="pt-[2rem]">1</p>
-                            <h4>Siguiendo</h4>
+                            <button className="bg-[#E0E1E3] font-semibold rounded-xl px-[4rem] py-[0.5rem] mt-[1rem] lg:mt-[0rem] dark:text-white">Compartir</button>
+                            <p className="pt-[2rem] dark:text-white">1</p>
+                            <h4 className="dark:text-white">Siguiendo</h4>
                         </div>
 
                         <div className="grid grid-cols-2 gap-3 lg:hidden">
@@ -82,18 +82,18 @@ export function PersonalInformation() {
 
                         <div className="grid grid-cols-3 gap-3 lg:hidden">
                             <div>
-                                <p className="pt-[2rem]">1</p>
-                                <h4>Publicaciones</h4>
+                                <p className="pt-[2rem] dark:text-white">1</p>
+                                <h4 className="dark:text-white">Publicaciones</h4>
                             </div>
 
                             <div>
-                                <p className="pt-[2rem]">1</p>
-                                <h4>Seguidores</h4>
+                                <p className="pt-[2rem] dark:text-white">1</p>
+                                <h4 className="dark:text-white">Seguidores</h4>
                             </div>
 
                             <div>
-                                <p className="pt-[2rem]">1</p>
-                                <h4>Siguiendo</h4>
+                                <p className="pt-[2rem] dark:text-white">1</p>
+                                <h4 className="dark:text-white">Siguiendo</h4>
                             </div>
                         </div>
                     </div>
