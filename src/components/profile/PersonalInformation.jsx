750928a02c8b1e62b235af93c99a32b0ce054932
tintin1401 <dana.cardenascr@gmail.com>
2024-09-22 21:26:16 -0600

    dark mode profile

--- src/components/profile/PersonalInformation.jsx
+++ src/components/profile/PersonalInformation.jsx
@@ -4,7 +4,7 @@ import { Navigation } from "../navigation/Navigation";
 import { SearchDropdown } from "../dropdown/SearchDropdown";
 import { CarouselCard } from "../carousel/CarouselCard";
 import { useFetchMenubar } from "../hooks/useFetchMenubar.js";
-
+import { useEffect } from "react";
 import { CardInformation } from "../cards/CardInformation";
 ("use client");
 
@@ -16,6 +16,21 @@ export function PersonalInformation() {
 
     const [isOpen, setIsOpen] = useState(false);
     const handleClose = () => setIsOpen(false);
+    const [darkMode, setDarkMode] = useState(true);
+
+    useEffect(() => {
+        if (darkMode) {
+          document.body.classList.add('dark');
+        } else {
+          document.body.classList.remove('dark');
+        }
+      }, [darkMode]);
+      
+        const toggleDarkMode = () => {
+          console.log(darkMode);
+          setDarkMode(!darkMode);
+        };
+
     const [id, setId] = useState(0);
     const openCard = (id) => () => {
         setIsOpen(true);
@@ -25,10 +40,10 @@ export function PersonalInformation() {
 
     return (
 
-        <div className="flex overflow-x-hidden">
+        <div className="flex overflow-x-hidden dark:bg-[#2a2a2a]">
 
             <div className="flex-shrink-0 fixed top-0 left-0 z-10 h-full">
-                <Navigation />
+                <Navigation darkMode={toggleDarkMode}/>
             </div>
 
             <Drawer open={isOpen} onClose={handleClose} position="right">
@@ -42,7 +57,7 @@ export function PersonalInformation() {
                     marginLeft: isMobile ? '0px' : '80px',
                 }}>
                 <div className="flex pt-4 justify-between">
-                    <h1 className="font-black text-3xl lg:text-4xl mt-2">ESCAPE</h1>
+                    <h1 className="font-black text-3xl lg:text-4xl mt-2 dark:text-white">ESCAPE</h1>
                     <SearchDropdown />
                 </div>
 
@@ -64,13 +79,13 @@ export function PersonalInformation() {
                         </div>
 
                         <div className="hidden lg:block">
-                            <button className="bg-[#E0E1E3] font-semibold rounded-xl px-[4rem] py-[0.5rem] mt-[1rem] lg:mt-[0rem] dark:text-white">Editar</button>
+                            <button className="bg-[#E0E1E3] font-semibold rounded-xl px-[4rem] py-[0.5rem] mt-[1rem] lg:mt-[0rem] dark:text-white dark:bg-[#404040]">Editar</button>
                             <p className="pt-[2rem] dark:text-white">1</p>
                             <h4 className="dark:text-white">Seguidores</h4>
                         </div>
 
                         <div className="hidden lg:block">
-                            <button className="bg-[#E0E1E3] font-semibold rounded-xl px-[4rem] py-[0.5rem] mt-[1rem] lg:mt-[0rem] dark:text-white">Compartir</button>
+                            <button className="bg-[#E0E1E3] font-semibold rounded-xl px-[4rem] py-[0.5rem] mt-[1rem] lg:mt-[0rem] dark:text-white dark:bg-[#404040]">Compartir</button>
                             <p className="pt-[2rem] dark:text-white">1</p>
                             <h4 className="dark:text-white">Siguiendo</h4>
                         </div>
@@ -100,7 +115,7 @@ export function PersonalInformation() {
                 </div>
 
                 <div className="mt-10 pb-[5rem]">
-                    <h2 className="font-bold lg:text-2xl text-xl mb-8">
+                    <h2 className="font-bold lg:text-2xl text-xl mb-8 dark:text-white">
                         Post
                     </h2>
                     <CarouselCard setIsOpen={openCard} />
