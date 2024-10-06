2d76bb5b3c92c730ae7a8003499538fedea16e1a
AsuncionPD <asuncion.pena@ucr.ac.cr>
2024-10-06 13:18:02 -0600

    Modified card information by adding traveltime
    Se agregó el tiempo de viaje en modo caminata

--- src/components/cards/CardInformation.jsx
+++ src/components/cards/CardInformation.jsx
@@ -1,24 +1,82 @@
+import { useState, useEffect } from "react";
 import "../../index.css";
-import a from "../../assets/imgs/Place1.jpg";
+//import a from "../../assets/imgs/Place1.jpg";
 import start from "../../assets/imgs/start.svg";
 import location from "../../assets/imgs/location.svg";
 import money from "../../assets/imgs/money.svg";
 import guide from "../../assets/imgs/guide.svg";
 import heart from "../../assets/imgs/heart.svg";
 import back from "../../assets/imgs/back.svg";
+import useFetchData from "../hooks/useFetchData";
+import { useUser } from '../../context/UserContext.jsx';
 
 import propTypes from "prop-types";
 
 export function CardInformation({ id , onClose}) {
+
+const url = `http://localhost/escape-desarrollo-backend/public/api/companies/${id}`;
+
+const { data: placeData, loading, error } = useFetchData(url);
+const { user } = useUser();
+
+const [travelTime, setTravelTime] = useState(null);
+
+console.log(placeData);
+
+useEffect(() => {
+  if (!placeData || !placeData[0] || !user) return;
+
+  const place = placeData[0];
+  
+  if (place.longitude && place.latitude && user.longitude && user.latitude) {
+    const origin = [user.longitude, user.latitude];
+    const destination = [place.longitude, place.latitude];
+
+    const routeUrl = `https://api.tomtom.com/routing/1/calculateRoute/${origin[1]},${origin[0]}:${destination[1]},${destination[0]}/json?key=dd8qO1N1bSR7yu4ShWlBi4HDup4MKSwi&traffic=false&travelMode=pedestrian`;
+
+    fetch(routeUrl)
+      .then(response => response.json())
+      .then(data => {
+        const travelTimeInSeconds = data.routes[0].summary.travelTimeInSeconds;
+        const travelTimeInMinutes = Math.round(travelTimeInSeconds / 60);
+        const travelTimeFormatted = convertirMinutosAHoras(travelTimeInMinutes);
+        setTravelTime(travelTimeFormatted);
+      })
+      .catch(error => console.error('Error al calcular la ruta:', error));
+  } else {
+    setTravelTime('Calculando...');
+  }
+}, [placeData, user]);
+
+  function convertirMinutosAHoras(minutos) {
+    const horas = Math.floor(minutos / 60);
+    const minutosRestantes = minutos % 60;
+    return `${horas}h ${minutosRestantes}m`;
+  }
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error fetching data: {error.message}</p>;
+  }
+
+  if (!placeData || !placeData[0]) {
+    return <p>No data found</p>;
+  }
+
+  const place = placeData[0];
+
   return (
     <div>
         <div className="relative">
-        <img className="rounded-lg object-cover h-[50vh]  md:w-full" src={a} alt="" />
+        <img className="rounded-lg object-cover h-[50vh]  md:w-full" src={(`/src/assets/imgs/${place.image}`)} alt="" />
         <img className="absolute top-4 right-4 p-2 rounded-lg bg-white dark:bg-[#404040]" src={heart} alt="" />
         <img onClick={onClose} className="absolute top-2 left-2 p-2 rounded-full" src={back} alt="" />
         </div>
       <div className="flex justify-between mt-4">
-        <h3 className="text-black font-semibold text-3xl dark:text-white">Soda Maria {id}</h3>
+        <h3 className="text-black font-semibold text-3xl dark:text-white">{place.name}</h3>
         <div className="flex items-center gap-1">
           <img src={start} alt="start" />
           <p className="text-[#9A9797] font-semibold text-xl dark:text-[#BCBCBC]">
@@ -29,7 +87,7 @@ export function CardInformation({ id , onClose}) {
       <div className="flex items-center gap-2 my-3">
         <img src={location} alt="location" />
         <p className="text-[#9A9797] font-semibold text-xl dark:text-[#BCBCBC]">
-          Esparza Centro
+          {place.canton_id}
         </p>
       </div>
       <div className="flex items-center justify-between bg-white dark:bg-[#404040] dark:border-gray-600 border border-gray-200 rounded-lg shadow  ">
@@ -45,11 +103,11 @@ export function CardInformation({ id , onClose}) {
         <div className="flex items-center gap-4 my-3 mx-8">
           <img src={guide} alt="location" />
           <p className="text-black font-semibold text-xl md:text-2xl dark:text-[#BCBCBC]">
-            7  <span className="sm:text-base md:text-2xl">min</span>
+          {travelTime ? travelTime : 'Calculando...'}
           </p>
         </div>
       </div>
-      <p className="mt-4 dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. At minima quibusdam illum exercitationem iste quos provident, quo in tenetur vero facilis omnis dolorem, beatae alias labore eos laboriosam saepe pariatur.</p>
+      <p className="mt-4 dark:text-white">{place.description}</p>
       <div className="flex align-bottom ">
       <button className="w-full bg-sky-500 text-white font-bold py-2 rounded-lg mt-4 text-lg">Ir</button>
 
--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -16,13 +16,11 @@ export function Sidebar({darkMode}) {
                      style={{ width: "50px" }} 
                 />
             </div>
-            <button className="mt-[10px] mb-[10px] w-full h-[45px] flex justify-center items-center rounded-lg bg-[#FFD8E4] hover:bg-[#f8c3d4] dark:hover:bg-[##404040] transition-colors duration-300 cursor-pointer">
-                <a href="/create-post">
+            <a href="/create-post" className="mt-[10px] mb-[10px] w-full h-[45px] flex justify-center items-center rounded-lg bg-[#FFD8E4] hover:bg-[#f8c3d4] dark:hover:bg-[##404040] transition-colors duration-300 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
                 <span className={`ml-2 flex transition-all duration-500 whitespace-nowrap ${sidebarWidth === "80px" ? 'hidden' : 'inline'}`}>Create new</span>
-                </a>
-            </button>
+            </a>
             <nav>
                 <ul>
                     <li className="flex mb-[10px] mt-[10px]">
